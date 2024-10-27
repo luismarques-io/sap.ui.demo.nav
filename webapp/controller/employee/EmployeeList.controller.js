742ac50dcb12221bf42f1a5aa2ba2e5b82309777
Luís Marques <l.marques@sap.com>
2024-10-27 18:44:00 +0000

    Implement employee detail view and navigation; add Employee controller, view, and update routing

--- webapp/controller/employee/EmployeeList.controller.js
+++ webapp/controller/employee/EmployeeList.controller.js
@@ -1,4 +1,13 @@
 sap.ui.define(["sap/ui/demo/nav/controller/BaseController"], function (BaseController) {
   "use strict";
-  return BaseController.extend("sap.ui.demo.nav.controller.employee.EmployeeList", {});
+  return BaseController.extend("sap.ui.demo.nav.controller.employee.EmployeeList", {
+    onListItemPressed: function (oEvent) {
+      var oItem, oCtx;
+      oItem = oEvent.getSource();
+      oCtx = oItem.getBindingContext();
+      this.getRouter().navTo("employee", {
+        employeeId: oCtx.getProperty("EmployeeID"),
+      });
+    },
+  });
 });
