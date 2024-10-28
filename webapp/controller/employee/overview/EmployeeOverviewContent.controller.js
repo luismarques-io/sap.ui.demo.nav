55d679cbfce99d1cd56a064f82b2fd7bf9dab117
Luís Marques <l.marques@sap.com>
2024-10-28 16:18:29 +0000

    Enhance employee overview feature; implement sorting functionality via URL hash and update navigation in view settings dialog

--- webapp/controller/employee/overview/EmployeeOverviewContent.controller.js
+++ webapp/controller/employee/overview/EmployeeOverviewContent.controller.js
@@ -26,8 +26,13 @@ sap.ui.define(
         this._oRouterArgs = oEvent.getParameter("arguments");
         this._oRouterArgs["?query"] = this._oRouterArgs["?query"] || {};
 
+        var oQueryParameter = this._oRouterArgs["?query"];
+
         // search/filter via URL hash
-        this._applySearchFilter(this._oRouterArgs["?query"].search);
+        this._applySearchFilter(oQueryParameter.search);
+
+        // sorting via URL hash
+        this._applySorter(oQueryParameter.sortField, oQueryParameter.sortDescending);
       },
 
       onSortButtonPressed: function () {
@@ -42,10 +47,15 @@ sap.ui.define(
       },
 
       _initViewSettingsDialog: function () {
+        var oRouter = this.getRouter();
+
         this._oVSD = new ViewSettingsDialog("vsd", {
           confirm: function (oEvent) {
             var oSortItem = oEvent.getParameter("sortItem");
-            this._applySorter(oSortItem.getKey(), oEvent.getParameter("sortDescending"));
+
+            this._oRouterArgs["?query"].sortField = oSortItem.getKey();
+            this._oRouterArgs["?query"].sortDescending = oEvent.getParameter("sortDescending");
+            oRouter.navTo("employeeOverview", this._oRouterArgs, true /*without history*/);
           }.bind(this),
         });
 
